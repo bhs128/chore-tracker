f591ec9c73921ba1ff6cef873f4b2eb072031a2b
Ben Schaffhausen <bhs128@gmail.com>
2026-03-17 22:14:46 -0500

    Fix journalctl logging: add SyslogIdentifier, drop redundant timestamp/level

--- server/server.py
+++ server/server.py
@@ -45,8 +45,7 @@ from urllib.parse import unquote, urlparse
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s  %(levelname)-8s  %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
+    format="%(message)s",
 )
 log = logging.getLogger("chore-server")
 
